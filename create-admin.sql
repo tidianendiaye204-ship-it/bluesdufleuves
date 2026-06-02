4dc9d89b3d26a4d28f95f73907849b3cb6a672b7
Babacar Mbaye <geniedevgeniedev@gmail.com>
2026-06-02 23:28:42 +0000

    qq

---
+++ create-admin.sql
@@ -0,0 +1,1 @@
+INSERT INTO admins (id, email, password_hash) VALUES ('f6b87d77-1b73-4701-8e60-81b925207d60', 'maboul1847@gmail.com', '$2b$12$lTthraDhxJkKlW/YRKFmNurUQAsuy9Ghz9CRGhPIPO6RdIQw6M/qm');
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,12 @@
+import bcrypt from "bcryptjs";
+
 export async function hashPassword(password: string): Promise<string> {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(password);
-  const hash = await crypto.subtle.digest("SHA-256", data);
-  return Array.from(new Uint8Array(hash))
-    .map((b) => b.toString(16).padStart(2, "0"))
-    .join("");
+  const saltRounds = 12;
+  return bcrypt.hash(password, saltRounds);
 }
 
 export async function verifyPassword(password: string, hash: string): Promise<boolean> {
-  const passwordHash = await hashPassword(password);
-  return passwordHash === hash;
+  return bcrypt.compare(password, hash);
 }
 
 export function generateId(): string {
