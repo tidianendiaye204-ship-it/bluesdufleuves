70ac5f2b78c44b148bfb12ba0a0a44adb777f752
Babacar Mbaye <geniedevgeniedev@gmail.com>
2026-07-17 22:22:36 +0000

    a

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,7 @@ export default defineConfig({
       preset: "cloudflare-pages",
     },
   },
+  define: {
+    __name: '((target, value) => Object.defineProperty(target, "name", { value, configurable: true }))',
+  },
 });
